1004fbc7916d7e6745b580747218cd6655c8b8b1
Melisa <acarme17@itu.edu.tr>
2024-10-08 15:23:55 +0300

    chore: formatting before automation part 1

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-import requests
-from bs4 import BeautifulSoup
-import pandas as pd
-import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException  # TimeoutException'ı içe aktar
+import time
 
 # Ay numaralarını ve isimleri eşle
 months_mapping = {
-    1: "Ocak",
-    2: "Şubat",
-    3: "Mart",
-    4: "Nisan",
-    5: "Mayıs",
-    6: "Haziran",
-    7: "Temmuz",
+    1: "OCAK SONU",
+    2: "ŞUBAT SONU",
+    3: "MART SONU",
+    4: "NİSAN SONU",
+    5: "MAYIS SONU",
+    6: "HAZİRAN SONU",
+    7: "TEMMUZ SONU",
     8: "AĞUSTOS SONU",
-    9: "Eylül",
-    10: "Ekim",
-    11: "Kasım",
-    12: "Aralık"
+    9: "EYLÜL SONU",
+    10: "EKİM SONU",
+    11: "KASIM SONU",
+    12: "ARALIK SONU"
 }
 
 # Kullanıcının istediği ayı seçmesi
-month_num = 8  # Buraya istenilen ay numarası yazılır
+month_num = 8  
 
 if month_num in months_mapping:
     month_name = months_mapping[month_num]
@@ -28,51 +30,35 @@ else:
     print("Geçersiz ay")
     exit()
 
-
 def download_excel(month_name):
-    url = 'https://www.dhmi.gov.tr/Sayfalar/Istatistikler.aspx'
-    r = requests.get(url)
+    driver = webdriver.Safari()
+    driver.get('https://www.dhmi.gov.tr/Sayfalar/Istatistikler.aspx')
 
-    # Eğer sayfaya erişim sağlanamazsa
-    if r.status_code != 200:
-        print("Sayfaya erişim sağlanamadı.")
-        return
+    wait = WebDriverWait(driver, 10)  # 10 saniye bekle
+    
+    try:
+        # Ay ismi içeren satırı bulma
+        row = wait.until(EC.presence_of_element_located((By.XPATH, f"//*[contains(text(), '{month_name}')]")))
+        
+        if row:
+            parent_row = row.find_element(By.XPATH, './ancestor::tr')  # Satırın parentını bul
+            
+            download_buttons = parent_row.find_elements(By.XPATH, ".//a[span[text()='Tümünü İndir']]")
 
-    #Sayfa içeriğini analiz etme
-    soup = BeautifulSoup(r.content, 'html.parser')
-
-    #Ay ismi içeren satır bulunur
-    row = soup.find(string=month_name)
-
-    #Eğer satır bulunduysa
-    if row:
-        parent_row = row.find_parent("tr") #satırın parenti bulunur
-        #print("Parent Element İçeriği:", parent_row.prettify())
-        #button = parent_row.find("a", string = "Tümünü indir") #Buton bulunur
-        if parent_row:
-            #Parent satırındaki tüm <a> etkiketlerini bul
-            all_links = parent_row.find_all("a")
-
-            #7. butonu al, which is tümünü indir
-            if len(all_links) >= 7:
-                download_button = all_links[6]
-                download_url = download_button['href'] #İndirme linki
-                print("İndirme linki", download_url)
-
-                #dosya indirilir
-                excel_response = requests.get(download_url)
-                if excel_response.status_code == 200:
-                    with open("deneme_dosya.xlsx", "wb") as f:
-                        f.write(excel_response.content)
+            if download_buttons:
+                for button in download_buttons:
+                    download_url = button.get_attribute('href')  # İndirme linki
+                    print("İndirme linki:", download_url)
+                    
+                    button.click()  # Dosyayı indirmek için butona tıklayın
                     print("Dosya indirildi")
-                else:
-                    print("Dosya indirilemedi")
+                    time.sleep(2)  # İndirme süresini beklemek için
             else:
                 print("Tümünü indir butonu bulunamadı")
-        else:
-            print("Satırın parent elementi bulunamadı")
-    else:
-        print("Satır bulunamadı")
+    except TimeoutException:
+        print(f"{month_name} için satır bulunamadı. Sayfa zaman aşımına uğradı.")
+    finally:
+        driver.quit()  # Tarayıcıyı kapat
 
 # Fonksiyonu çağır
 download_excel(month_name)
