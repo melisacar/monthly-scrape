3e44267b0b93af45278a965a4b17d268829eebb4
Melisa <acarme17@itu.edu.tr>
2024-10-16 16:46:01 +0300

    feat: checks if new month added

--- dhmi_check4_month.py
+++ dhmi_check4_month.py
@@ -61,66 +61,113 @@ def parse_excel_links(html_content):
     hrefs = [element.get('href') for element in elements]
     return hrefs
 
-###########################################################################################################
-#def check_last_month(html_content):
+def extract_month_number(month_string):
     """
-    Parses the HTML content to find the last month available.
-    Returns the index of the last month found (0-based, where 0=January).
+    Extracts the month number from the month string (e.g. "TEMMUZ SONU"). 
+    Returns the month number as an integer.
     """
-    soup = BeautifulSoup(html_content, 'html.parser')
+    for month, name in months_mapping.items():
+        if month_string.strip().upper() == name:
+            return month
+    return None
 
-    # Find all table data with the class "text-left", example: <td class="text-left">EYLÜL SONU</td>
-    month_cells = soup.find_all(
-        'td',
-        class_='text-left'
- )
-###########################################################################################################
+def get_latest_month_excel(file_path):
+    """
+    Gets the latest month number from the Excel file.
+    """
+    try:
+        last_month_str = pd.read_excel(file_path).iloc[-1]['Tarih']
+        month_number = last_month_str.split("-")[1]
+        return int(month_number)
+    except FileNotFoundError:
+        return 0
 
+def find_newest_month_html(html_content):
+    """
+    Checks the month texts from HTML content and returns the largest month number.
+    """
+    soup = BeautifulSoup(html_content, "html.parser")
+    month_numbers = []
 
+    td_elements = soup.find_all(
+        'td', class_="text-left"
+        )
 
+    for td in td_elements:
+        month_string = td.get_text()
+        month_number = extract_month_number(month_string)
+        if month_number:
+            month_numbers.append(month_number)
 
+    return max(month_numbers, default=None) # Returns newest month.
 
 def download_excel_file(href):
     """
     Downloads the Excel file from the given href.
     Returns the content of the file if successful, else None.
     """
-    # Encode the URL to handle non-ASCII characters
+    # Encode the URL to handle non-ASCII characters.
     encoded_href = urllib.parse.quote(href, safe=':/')
     # Make the request while ignoring SSL verification
     response = requests.get(encoded_href, verify=False)
     if response.status_code == 200:
-        #print(f"Downloaded file size: {len(response.content)} bytes") # Dosya boyutunu yazdır (kontrol-1).
+        #print(f"Downloaded file size: {len(response.content)} bytes") # Print the size of the downloaded file (for control).
         return response.content
     else:
         print(f"Failed to retrieve {href}, status code: {response.status_code}") 
         return None
 
+def extract_year_month(date_info):
+    """
+    Extracts the year and month from the given date information string.
+    """
+    # Map Turkish month names to numerical values
+    month_mapping = {
+        "OCAK": "01", "ŞUBAT": "02", "MART": "03", "NİSAN": "04",
+        "MAYIS": "05", "HAZİRAN": "06", "TEMMUZ": "07", "AĞUSTOS": "08",
+        "EYLÜL": "09", "EKİM": "10", "KASIM": "11", "ARALIK": "12"
+    }
+    # Split the date_info to get the year and month part
+    parts = date_info.split()
+    year = parts[0]  # The first part is the year, e.g., "2024"
+    month_text = parts[1]  # The second part is the month in text, e.g., "MART"
+    
+    # Convert month text to its corresponding number
+    month = month_mapping.get(month_text.upper(), "01")  # Default to "01" if month not found
+    
+    return f"{year}-{month}"
+
 def transform_excel_file(excel_content):
     """
     Reads the Excel content into a pandas DataFrame and processes it.
     """
-    sheets_dict = pd.read_excel(BytesIO(excel_content), sheet_name=None) # Get all sheets.
+    sheets_dict = pd.read_excel(BytesIO(excel_content), sheet_name=None) # Read all sheets into a dictionary.
     
     all_sheets = []
 
     for sheet_name, sheet_data in sheets_dict.items():
         
-        additional_info = sheet_data.iloc[0, 4]  # Select the date cell.
+        additional_info = sheet_data.iloc[0, 4]  # Select the date cell from the first row.
+        formatted_date = extract_year_month(additional_info)  # Convert to "YYYY-MM" format.
 
-        total_rows = sheet_data.shape[0]
+        # Find the row number that contains "DHMİ TOPLAMI" phrase.
+        dhmi_toplami_index = sheet_data[sheet_data.iloc[:,0].str.contains("DHMİ TOPLAMI", na=False)].index.min() #DHMI TOPLAM ifadesinin geçtiği ilk satırı alır.
 
-        processed_data = sheet_data.iloc[2:total_rows-8, [0, 4, 5, 6]]
+        # If "DHMİ TOPLAMI" is found, take the data up to this row.
+        # If not found, take up to the 9th row from the end.
+        end_row = dhmi_toplami_index if dhmi_toplami_index else sheet_data.shape[0] - 8
+
+        processed_data = sheet_data.iloc[2:end_row, [0, 4, 5, 6]]
         processed_data.columns = ['Havalimanı', 'İç Hat', 'Dış Hat', 'Toplam']
         processed_data.fillna(0, inplace=True) 
-        processed_data['Kategori'] = sheet_name # Add sheet names as a column.
-        processed_data['Tarih'] = additional_info  # Add date as a new column.
+        processed_data['Kategori'] = sheet_name # # Add sheet names as a new column. 
+        processed_data['Tarih'] = formatted_date  # Add the formatted date.
 
-        all_sheets.append(processed_data) # Append the processed DataFrame to the all_sheets list.
+        all_sheets.append(processed_data) # Append the processed DataFrame to the list.
     
-    merged_all_sheets = pd.concat(all_sheets) # Concatenate all dfs.
+    merged_all_sheets = pd.concat(all_sheets) # Concatenate all DataFrames into one.
 
-    final_data = []
+    final_data = [] # List to store final transformed data.
 
     for index, row in merged_all_sheets.iterrows():
         havalimani = row['Havalimanı']
@@ -130,77 +177,47 @@ def transform_excel_file(excel_content):
         kategori = row['Kategori']
         tarih = row['Tarih']
 
-        final_data.append([havalimani, 'İç Hat', ic_hat, kategori, tarih]) # The append function takes a single parameter, so append this as a list.
+        final_data.append([havalimani, 'İç Hat', ic_hat, kategori, tarih]) # Append function takes single parameter, thus take as a list.
         final_data.append([havalimani, 'Dış Hat', dis_hat, kategori, tarih])
         final_data.append([havalimani, 'Toplam', toplam, kategori, tarih])
     
 
-    final_df = pd.DataFrame(final_data, columns=['Havalimanı', 'Hat Türü', 'Num', 'Kategori', 'Tarih']) # Converted to df because it will be concatenated in the main function.
+    final_df = pd.DataFrame(final_data, columns=['Havalimanı', 'Hat Türü', 'Num', 'Kategori', 'Tarih']) # Transformed to df to use concat in main() func.
     return final_df
 
-def dhmi_scrape():
-    # Disable SSL warnings
+def main_check():
+    # Disable SSL warnings.
     disable_ssl_warnings()
 
-    # URL of the site
+    # URL of the site.
     url = "https://www.dhmi.gov.tr/Sayfalar/Istatistikler.aspx"
 
     # Fetch the page content
     html_content = fetch_page_content(url)
     if not html_content:
-        return  # Exit if the page couldn't be retrieved
+        return  # Exit if the page couldn't be retrieved.
 
-    # Parse the HTML to get Excel links
+    # Parse the HTML to get Excel links.
     hrefs = parse_excel_links(html_content)
 
-    all_data = []  # List to hold all df DataFrames
+    # Get the last month number from existing data.
+    latest_month = get_latest_month_excel('DHMI_all.xlsx')
 
-    # Loop through the URLs and process each Excel file
-    for href in hrefs:
-        print(f"Processing file from {href}")
-        excel_content = download_excel_file(href)
-        if excel_content:
-            df = transform_excel_file(excel_content)  # Transform the file.
-            all_data.append(df)  # Append each df to the list.
-        else:
-            print(f"Skipping {href} due to download error.")
-    
-    # Concatenate all DataFrames into a single DataFrame.
-    if all_data:
-        final_df = pd.concat(all_data, ignore_index=True)
+    # Get the latest month number from html.
+    newest_month = find_newest_month_html(html_content)
+    print(latest_month)
 
-        # Save the final DataFrame to a single Excel file.
-        final_df.to_excel("DHMI_all.xlsx", index=False)
-        print("Data has been saved to 'DHMI_all.xlsx'.")
+    if newest_month and newest_month > latest_month:  
+        for href in hrefs:
+            print(f"Yeni dosya işleniyor: {href}")
+            excel_content = download_excel_file(href)
+            if excel_content:
+                df = transform_excel_file(excel_content) 
+                print("Yeni veri bulundu. Mevcut dosyaya ekleniyor...")
+                # Using ExcelWriter to append the DataFrame to the existing Excel file
+                with pd.ExcelWriter("DHMI_all.xlsx", engine='openpyxl', mode='a') as writer:
+                    df.to_excel(writer, header=False, index=False)  # Add.
+            else:
+                print(f"{href} indirme hatasından dolayı atlandı.")
 
-
-#Cron Job using schedule lib.
-def job():
-    """
-    Job to run on schedule.
-    """
-    print("Job started...")
-    dhmi_scrape()
-    print("Job completed.")    
-
-#Use tz identifier Europe/Istanbul for UTC+3.
-timezone = pytz.timezone("Europe/Istanbul")
-local_time = da0tetime.now(timezone).strftime("%H:%M")
-
-# Schedule the job to run every day at a specific time.
-schedule.every().weekday.at("10:30").do(job)
-
-while True:
-    current_date = datetime.now(timezone)
-
-     # The last day of the first month of the next year.
-    end_date = datetime(datetime.year + 1, 1, 31)
-
-    # If the current date is before January 31 of the next year, execute the job.
-    if current_date < end_date:
-        schedule.run_pending()
-    else:
-        print("Job stopped.")
-        break
-    
-    tm.sleep(1)
+main_check()
