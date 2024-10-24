778467aef034ff31207b18d26b870c6eae551757
Melisa <acarme17@itu.edu.tr>
2024-10-24 16:42:07 +0300

    feat: add models file for type and conn

--- models.py
+++ models.py
@@ -1,5 +1,27 @@
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, Float, String, Date
 
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 Base = declarative_base()
-metadata = Base.metadata
+
+# Define the Flights table.
+class Flight(Base):
+    __tablename__ = 'flights' 
+
+    id=Column(Integer, primary_key=True, autoincrement=True)  
+    havalimani=Column(String, nullable=True)
+    hat_turu=Column(String, nullable=True)
+    num=Column(Float, nullable=True)
+    kategori=Column(String, nullable=True)
+    tarih=Column(Date, nullable=True)
+    #retrieved_at = Column(Date, nullable=True) 
+
+# Database connection.
+engine = create_engine('postgresql://postgres:secret@localhost:5432/dhmi-scrape')
+
+# Create tables in the database.
+Base.metadata.create_all(engine)
+
+# Create a session for adding data.
+Session = sessionmaker(bind=engine)
