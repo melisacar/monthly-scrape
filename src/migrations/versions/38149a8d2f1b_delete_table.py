3e91ffb3bdd3c3b48b4959a7b64589f42598b2d5
Melisa ACAR <melisa.acar@ipa.istanbul>
2024-11-29 09:19:07 +0300

    ch: edit first migration

---
+++ src/migrations/versions/38149a8d2f1b_delete_table.py
@@ -0,0 +1,26 @@
+"""Delete table
+
+Revision ID: 38149a8d2f1b
+Revises: 
+Create Date: 2024-10-24 12:30:05.557087
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = '38149a8d2f1b'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    op.execute('DROP TABLE IF EXISTS turizm.tum_ucuslar_aylik')
+
+
+def downgrade() -> None:
+    pass
