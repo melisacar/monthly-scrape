6464b44c91d451d40fd2f7a5532b6c430d0f74d3
Melisa ACAR <melisa.acar@ipa.istanbul>
2024-11-29 15:08:21 +0300

    ft: testing for default env and url

--- src/config.py
+++ src/config.py
@@ -1,16 +1,20 @@
 # Define url manually:
-DATABASE_URL = ("postgresql://postgres:secret@localhost:5432/dhmi-scrape") # While using Alembic
+#DATABASE_URL = ("postgresql://postgres:secret@localhost:5432/dhmi-scrape") # While using Alembic
 #DATABASE_URL = ("postgresql://postgres:secret@database:5432/dhmi-scrape") # While using Docker
 
+import os
+# Get the environment
+env = os.getenv("ENV", "local")
 
-#import os
+# Set the default DATABASE_URL if not provided
+if env == "docker":
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:secret@database:5432/dhmi-scrape")
+elif env == "local":
+    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:secret@localhost:5432/dhmi-scrape")
+#elif env == "prod":
+#    DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:secret@proddb:5432/dhmi-scrape")
+else:
+    DATABASE_URL = os.getenv("DATABASE_URL")
 
-# Toggle between Alembic and Docker
-#USE_DOCKER = True  # True if using Docker, False if using Alembic locally
-
-#if USE_DOCKER:
-#    DATABASE_URL = "postgresql://postgres:secret@database:5432/dhmi-scrape"  # Docker
-#else:
-#    DATABASE_URL = "postgresql://postgres:secret@localhost:5432/dhmi-scrape"  # Localhost
-
-#print(f"Kullanılan DATABASE_URL: {DATABASE_URL}")
+if DATABASE_URL is None:
+    raise Exception("DATABASE_URL not found in environment")
