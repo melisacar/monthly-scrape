5bc73a2df455a1c8f6b136c1c792dff9a09d5ce4
Melisa ACAR <melisa.acar@ipa.istanbul>
2024-11-29 09:15:26 +0300

    ft: import url from config file

--- src/models.py
+++ src/models.py
@@ -3,13 +3,20 @@ from sqlalchemy import Column, Integer, Float, String, Date
 
 from sqlalchemy.orm import declarative_base, sessionmaker
 
+from sqlalchemy import UniqueConstraint
+
+from config import DATABASE_URL
+
 Base = declarative_base()
 
 # Define the Flights Check table.
 class Flight_Check(Base):
     __tablename__ = 'tum_ucuslar_aylik' 
-    __table_args__ = {'schema': 'turizm'}
-    
+    __table_args__ = (
+                      UniqueConstraint('havalimani', 'hat_turu','kategori', 'tarih', name='unique_ucuslar'), # Avoid duplicate data.
+                      {'schema': 'turizm'}
+                      )
+
     id=Column(Integer, primary_key=True, autoincrement=True)  
     havalimani=Column(String, nullable=True)
     hat_turu=Column(String, nullable=True)
@@ -20,7 +27,15 @@ class Flight_Check(Base):
 
 # Database connection.
 #engine = create_engine('postgresql://postgres:secret@localhost:5432/dhmi-scrape')
-engine = create_engine('postgresql://postgres:secret@database:5432/dhmi-scrape')
+#engine = create_engine('postgresql://postgres:secret@database:5432/dhmi-scrape')
+engine = create_engine(DATABASE_URL)
+
+
+#Create the schema if it doesn't exist
+#with engine.connect() as connection:
+#   connection.execute("""
+#                            CREATE SCHEMA IF NOT EXISTS turizm;
+#                      """)
 
 # Create tables in the database.
 Base.metadata.create_all(engine)
