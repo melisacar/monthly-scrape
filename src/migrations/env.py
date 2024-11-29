6a23177f10aa0f42c435f6b7cf6c094e857cdfc9
Melisa ACAR <melisa.acar@ipa.istanbul>
2024-11-29 10:46:16 +0300

    ch: switching to manual url localhost

--- src/migrations/env.py
+++ src/migrations/env.py
@@ -8,11 +8,17 @@ from sqlalchemy import create_engine
 from models import Base  # models.py import
 from logging.config import fileConfig
 
-import os
+#import os
+
+#DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+DATABASE_URL = 'postgresql://postgres:secret@localhost:5432/dhmi-scrape'
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option('sqlalchemy.url', DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
@@ -27,14 +33,11 @@ if config.config_file_name is not None:
 # add your model's MetaData object here
 target_metadata = Base.metadata 
 
-# Use DATABASE_URL from environment variables
-DATABASE_URL = os.getenv("DATABASE_URL", config.get_main_option("sqlalchemy.url"))
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
-
 
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
@@ -48,9 +51,10 @@ def run_migrations_offline() -> None:
     script output.
 
     """
+
     url = config.get_main_option("sqlalchemy.url")
     context.configure(
-        url=DATABASE_URL,
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
