69013990850bc011810e6b6ad24beb3b91ccf39a
Melisa ACAR <melisa.acar@ipa.istanbul>
2024-12-16 09:30:09 +0300

    chore: ignore old building files

--- build-files/migration.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS flights (
-    airport_name        VARCHAR,
-    fligt_type          VARCHAR,
-    quantity            FLOAT,
-    category            VARCHAR,
-    fligt_time          CHAR(7),
-    retrieval_at        DATE
-);
